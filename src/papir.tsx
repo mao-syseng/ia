78bb0511d2f28afce9ee906fe31cfa221c434cac
Marc Obel <mao@syseng.dk>
2026-02-19 16:11:38 +0100

    a

--- src/papir.tsx
+++ src/papir.tsx
@@ -1,7 +1,54 @@
 export default function papir() {
   return (
     <>
-      <h1>Papir</h1>
+      <h1>
+        Krea papir tryllestav som kan folde sig ud til en vifte og som alle børn
+        elsker
+      </h1>
+      <h2>Det du skal bruge</h2>
+      <ol>
+        <li>A4 papir</li>
+        <li>Saks</li>
+        <li>Lim</li>
+        <li>
+          Elastisk snor{" "}
+          <small>(Samme som til at lave perle armbånd virker godt)</small>
+        </li>
+      </ol>
+      <h2>Instruktioner</h2>
+      <p>Klip A4 papir over på midten</p>
+      <pre>{`┌──────────────────────────────────────┐
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+│                   │                  │
+│                                      │
+└───────────────────┴──────────────────┘`}</pre>
     </>
   );
 }
