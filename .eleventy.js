68b7e5a59a467f1c79dee58c01fecfb0d1a06378
Marc Obel <mao@syseng.dk>
2026-03-17 07:58:02 +0100

    a

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,3 @@
 export default function(eleventyConfig) {
-  // Copy the public folder to _site
   eleventyConfig.addPassthroughCopy("public");
-
-  // Ignore files
-  eleventyConfig.ignores.add("readme.md");
-  eleventyConfig.ignores.add("unicode.html");
 };
